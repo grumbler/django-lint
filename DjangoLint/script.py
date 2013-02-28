d2a8625cd4a5ac968088668a61b143f6bdc43e32
grumbler <serge.travin@gmail.com>
2013-02-28 13:17:23 +0400

    First step to compatibility with django-jenkins

--- DjangoLint/script.py
+++ DjangoLint/script.py
@@ -25,6 +25,72 @@ from optparse import OptionParser
 
 from DjangoLint import AstCheckers
 
+
+def djlint(rc_file, targets, errors_only=False, reporter=None, exit=False):
+    targets = [os.path.abspath(t) for t in targets]
+
+    for target in targets:
+        if not os.path.exists(target):
+            try:
+                # Is target a module?
+                x = __import__(args[0], locals(), globals(), [], -1)
+                target = sys.modules[args[0]].__path__[0]
+            except:
+                pass
+
+        if not os.path.exists(target):
+            raise parser.error(
+                "The specified target (%r) does not exist" \
+                % target
+            )
+
+        path = target
+        while True:
+            flag = False
+            for django_file in ('manage.py', 'models.py', 'urls.py'):
+                if os.path.exists(os.path.join(path, django_file)):
+                    sys.path.insert(0, os.path.dirname(path))
+                    flag = True
+                    break
+            if flag:
+                break
+
+            path = os.path.dirname(path)
+
+            if path == '/':
+                raise parser.error(
+                    "The specified target (%r) does not appear to be part of a " \
+                    "Django application" % target
+                )
+
+    try:
+        import django
+    except ImportError:
+        print >>sys.stderr, "E: Cannot import `django' module, exiting.."
+        return 1
+
+    linter = lint.PyLinter(reporter=reporter, pylintrc=rc_file)
+    #linter.set_option('reports', options.report)
+    #linter.set_option('output-format', options.outputformat)
+
+    if options.pylint:
+        checkers.initialize(linter)
+        for msg in ('C0111', 'C0301'):
+            linter.disable(msg)
+
+    AstCheckers.register(linter)
+
+    if errors_only:
+        linter.set_option('disable-msg-cat', 'WCRI')
+        linter.set_option('reports', False)
+        linter.set_option('persistent', False)
+
+    linter.check(targets)
+
+    return linter.msg_status
+
+
+
 def main():
     usage = """ %prog [options] target
 
